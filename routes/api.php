fed1c0c4debf4e4d23dd1f2b4ea06d3267876499
masaoshi <52434489+masaoshi@users.noreply.github.com>
2019-09-09 00:14:26 +0900

    ログイン処理のルート定義を追加

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Http\Request;
 // });
 // ユーザー登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
