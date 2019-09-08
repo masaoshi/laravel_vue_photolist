cb4af2046525b2220c55d53c8e15216554465c56
masaoshi <52434489+masaoshi@users.noreply.github.com>
2019-09-09 00:12:08 +0900

    ユーザー登録のルート定義を追加

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// ユーザー登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
