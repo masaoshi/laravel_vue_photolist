8329876db4d28e400f7d080a88720b275dbb8674
masaoshi <52434489+masaoshi@users.noreply.github.com>
2019-09-14 00:29:20 +0900

    ナビゲーションバーのスクリプトブロックを追加

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -20,4 +20,17 @@
             </div>
         </div>
     </nav>
-</template>>
+</template>
+
+<script>
+export default {
+    computed: {
+        isLogin () {
+            return this.$store.getters['auth/check']
+        },
+        username () {
+            return this.$store.getters['auth/username']
+        }
+    }
+}
+</script>
