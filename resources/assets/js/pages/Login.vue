4325160f551ec3780ca2bbd4be26d27f57ed8df9
masaoshi <52434489+masaoshi@users.noreply.github.com>
2019-09-17 02:11:57 +0900

    通信失敗時にトップページへの移動処理を行わない様に修正

--- resources/assets/js/pages/Login.vue
+++ resources/assets/js/pages/Login.vue
@@ -58,13 +58,20 @@ export default {
             }
         }
     },
+    computed: {
+        apiStatus () {
+            return this.$store.state.auth.apiStatus
+        }
+    },
     methods: {
         async login () {
             // auth ストアの login アクションを呼び出す
             await this.$store.dispatch('auth/login', this.loginForm)
 
-            // トップページに移動する
-            this.$router.push('/')
+            if (this.apiStatus) {
+                // トップページに移動する
+                this.$router.push('/')
+            }
         },
         async register () {
             // auth ストアの register アクションを呼び出す
